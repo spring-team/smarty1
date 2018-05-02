ab3b8446bcc585c2b08bc6f9125de1b38788e51b
Rod Johnson <johnsonroda@gmail.com>
2018-05-02 16:38:47 +1000

    Update Smarty1Controller.java

--- src/main/java/au/com/smartygrants/Smarty1Controller.java
+++ src/main/java/au/com/smartygrants/Smarty1Controller.java
@@ -27,7 +27,7 @@ class Smarty1Controller {
 
     @RequestMapping(method = GET, path = "/")
     public String root() {
-        return "Spring Boot REST service running in West Melbourne - Wed: Served by " + getClass().getName();
+        return "1: Spring Boot REST service running in West Melbourne - Wed: Served by " + getClass().getName();
     }
 
     @RequestMapping(method = GET, path = "hello/{name}")
